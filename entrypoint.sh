4a0018a0f6f5691876d4ff59a8c7500b85111e19
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2020-04-14 20:49:07 +0200

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,11 @@ fi
 
 USER=$(echo "$GITHUB_REPOSITORY" | cut -d "/" -f1 )
 PROJECT=$(echo "$GITHUB_REPOSITORY" | cut -d "/" -f2 )
-echo "$INPUT_USELATESTRELEASETAG"
+
 if [ "$INPUT_USELATESTRELEASETAG" = "true" ]; then
   LATEST_TAG=$(curl -s -H "Authorization: token $INPUT_TOKEN" "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/latest" | jq '.tag_name' | sed -e 's/"//g')
   if [ "$LATEST_TAG" = "null" ]; then LATEST_TAG=$(git describe --abbrev=0 --tags);fi;
   INPUT_CMD+=" --since-tag=$LATEST_TAG"
-  echo "debugging action"
 fi
 
 github_changelog_generator -u $USER -p $PROJECT --token $INPUT_TOKEN $INPUT_CMD
