756ca084b6eb7dfcf83624030bd8d7abc2ee517a
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2020-04-14 19:50:45 +0200

    support for useLatestReleaseTag flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,12 @@ fi
 USER=$(echo "$GITHUB_REPOSITORY" | cut -d "/" -f1 )
 PROJECT=$(echo "$GITHUB_REPOSITORY" | cut -d "/" -f2 )
 
+if [ "$INPUT_USELATESTRELEASETAG" = "yes" ]; then
+  LATEST_TAG=$(curl -s -H "Authorization: token $INPUT_TOKEN" "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/latest" | jq '.tag_name' | sed -e 's/"//g')
+  if [ "$LATEST_TAG" = "null" ]; then LATEST_TAG=$(git describe --abbrev=0 --tags);fi;
+  $INPUT_CMD+=" --since-tag=$LATEST_TAG"
+fi
+
 github_changelog_generator -u $USER -p $PROJECT --token $INPUT_TOKEN $INPUT_CMD
 
 FILE="CHANGELOG.md"
